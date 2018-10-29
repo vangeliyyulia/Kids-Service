94a2c7a9697b9c99abe33f66b15059aba73b2112
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-10-29 21:18:53 +0300

    Final

--- app/js/common.js
+++ app/js/common.js
@@ -78,8 +78,8 @@ kit.createLazy();
 function openForm() {
   document.getElementById("modal").style.display = "block";
 }
-
 function closeForm() {
   document.getElementById("modal").style.display = "none";
 }
+
 
